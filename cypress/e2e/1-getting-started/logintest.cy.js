a3a57826176c55da5925732f1c9bc365f22a3dc9
Emmanuel Okoye <talk2emmzy@gmail.com>
2023-08-31 11:59:02 +0100

    Made small change

--- cypress/e2e/1-getting-started/logintest.cy.js
+++ cypress/e2e/1-getting-started/logintest.cy.js
@@ -7,26 +7,17 @@ describe( 'Login test scenario', () =>{
 
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-        cy.title().should('eq',('OrangeHRM')) //Positive test case assertion 
-        .and('not.eq',('OrangeHRMM')) //Negative test case assertion 
+        cy.title().should('eq',('OrangeHRM')) //Positive test case assertion  
 
     })
 
-    it('verify that the user can login with valid email and password', ()=>{
+
+    it('verify that the user cannot login with valid email and password', ()=>{
 
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-        cy.title().should('eq',('OrangeHRM')) //Positive test case assertion 
-        .and('not.eq',('OrangeHRMM')) //Negative test case assertion 
+        cy.title().should('not.eq',('OrangeHRMM')) //Negative test case assertion 
 
     })
 
-    it('verify that the user can login with valid email and password', ()=>{
-
-        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
-        cy.title().should('eq',('OrangeHRM')) //Positive test case assertion 
-        .and('not.eq',('OrangeHRMM')) //Negative test case assertion 
-
-    })
 })
